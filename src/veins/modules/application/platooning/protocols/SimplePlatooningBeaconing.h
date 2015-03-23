ab6bb83018c689362be79a147ddea22676ca2db3
Michele Segata <segata@ccs-labs.org>
2015-03-23 17:12:09 +0100

    fix mispelled copyright

--- src/veins/modules/application/platooning/protocols/SimplePlatooningBeaconing.h
+++ src/veins/modules/application/platooning/protocols/SimplePlatooningBeaconing.h
@@ -1,5 +1,5 @@
 //
-// Copright (c) 2012-2015 Michele Segata <segata@ccs-labs.org>
+// Copyright (c) 2012-2015 Michele Segata <segata@ccs-labs.org>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
