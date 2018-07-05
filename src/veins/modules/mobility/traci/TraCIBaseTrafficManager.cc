127114db78086011b0a3dc79bc57b3acfaf3c582
Julian Heinovski <julian.heinovski@ccs-labs.org>
2018-07-05 18:06:35 +0200

    Make import absolute

--- src/veins/modules/mobility/traci/TraCIBaseTrafficManager.cc
+++ src/veins/modules/mobility/traci/TraCIBaseTrafficManager.cc
@@ -15,7 +15,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
-#include "TraCIBaseTrafficManager.h"
+#include "veins/modules/mobility/traci/TraCIBaseTrafficManager.h"
 
 Define_Module(TraCIBaseTrafficManager);
 
