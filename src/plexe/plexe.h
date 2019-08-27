91f8b3549c1803abb01003c027bc2b77ca3cf97f
Christoph Sommer <sommer@ccs-labs.org>
2019-08-27 14:07:12 +0200

    Add version management

--- src/plexe/plexe.h
+++ src/plexe/plexe.h
@@ -1,3 +1,36 @@
+//
+// Copyright (C) 2012-2019 Michele Segata <segata@ccs-labs.org>
+//
+// Documentation for these modules is at http://veins.car2x.org/
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
 #pragma once
 
-#include <veins/veins.h>
+#include "veins/veins.h"
+
+// Version number of last release ("major.minor.patch") or an alpha version, if nonzero
+#define PLEXE_VERSION_MAJOR 3
+#define PLEXE_VERSION_MINOR 0
+#define PLEXE_VERSION_PATCH 0
+#define PLEXE_VERSION_ALPHA 1
+
+// Explicitly check Veins version number
+#if !(VEINS_VERSION_MAJOR == 5 && VEINS_VERSION_MINOR >= 0)
+#error Veins version 5.0 or compatible required
+#endif
