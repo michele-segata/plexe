fc80f9d68c86c140a9903b0e921b60325945a7a5
Christoph Sommer <sommer@ccs-labs.org>
2019-08-27 19:00:13 +0200

    Add SPDX tags to each file, add explicit copyright headers where missing

--- src/plexe/CC_Const.h
+++ src/plexe/CC_Const.h
@@ -9,6 +9,8 @@
 // Copyright (C) 2012-2018 Michele Segata (segata@ccs-labs.org)
 /****************************************************************************/
 //
+//   SPDX-License-Identifier: GPL-2.0-or-later
+//
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation; either version 2 of the License, or
--- src/plexe/plexe.h
+++ src/plexe/plexe.h
@@ -1,23 +1,20 @@
 //
-// Copyright (C) 2012-2019 Michele Segata <segata@ccs-labs.org>
+// Copyright (c) 2012-2019 Michele Segata <segata@ccs-labs.org>
 //
-// Documentation for these modules is at http://veins.car2x.org/
+// SPDX-License-Identifier: LGPL-3.0-or-later
 //
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
 #pragma once
