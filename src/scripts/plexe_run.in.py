b3b746abc5bf2f5b195e69aa6989062612495cf9
Michele Segata <segata@ccs-labs.org>
2021-03-17 15:17:29 +0100

    general refactor of build and configuration files following new Veins specifications

--- src/scripts/plexe_run.in.py
+++ src/scripts/plexe_run.in.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python2
-
-# ^-- contents of out/config.py go here
+#!/usr/bin/env python3
 
 #
 # Copyright (C) 2013-2020 Christoph Sommer <sommer@ccs-labs.org>
@@ -25,19 +23,25 @@
 #
 
 """
-Runs Veins simulation in current directory
+Runs Plexe simulation in current directory
 """
 
+from __future__ import print_function
 import os
 import argparse
+import subprocess
+
+# v-- contents of out/config.py go here
+# ^-- contents of out/config.py go here
 
 def relpath(s):
-    veins_root = os.path.dirname(os.path.realpath(__file__))
+    veins_root = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
     return os.path.relpath(os.path.join(veins_root, s), '.')
 
-parser = argparse.ArgumentParser('Run a Veins simulation')
-parser.add_argument('-d', '--debug', action='store_true', help='Run using opp_run_dbg (instead of opp_run)')
-parser.add_argument('-t', '--tool', metavar='TOOL', dest='tool', choices=['lldb', 'gdb', 'memcheck'], help='Wrap opp_run execution in TOOL (lldb, gdb or memcheck)')
+parser = argparse.ArgumentParser('Run a Plexe simulation')
+parser.add_argument('-d', '--debug', action='store_true', help='Set --mode=debug (deprecated in favor of --mode)')
+parser.add_argument('-M', '--mode', metavar='MODE', dest='mode', choices=['', 'release', 'debug', 'sanitize'], help='Instead of opp_run, use opp_run_VARIANT corresponding to MODE (release, debug, sanitize)')
+parser.add_argument('-t', '--tool', metavar='TOOL', dest='tool', choices=['lldb', 'gdb', 'memcheck', 'callgrind'], help='Wrap opp_run execution in TOOL (lldb, gdb, memcheck, or callgrind)')
 parser.add_argument('-v', '--verbose', action='store_true', help='Print command line before executing')
 parser.add_argument('--', dest='arguments', help='Arguments to pass to opp_run')
 args, omnet_args = parser.parse_known_args()
@@ -50,7 +54,18 @@ run_imgs = [relpath(s) for s in run_imgs]
 
 opp_run = 'opp_run'
 if args.debug:
-    opp_run = 'opp_run_dbg'
+    args.mode = 'debug'
+if args.mode:
+    if args.mode == '':
+        opp_run = 'opp_run'
+    elif args.mode == 'release':
+        opp_run = 'opp_run_release'
+    elif args.mode == 'debug':
+        opp_run = 'opp_run_dbg'
+    elif args.mode == 'sanitize':
+        opp_run = 'opp_run_sanitize'
+    else:
+        assert False, 'unknown --mode option'
 
 lib_flags = ['-l%s' % s for s in run_libs]
 ned_flags = ['-n' + ';'.join(run_neds)]
@@ -63,11 +78,16 @@ if args.tool == 'gdb':
     prefix = ['gdb', '--args']
 if args.tool == 'memcheck':
     prefix = ['valgrind', '--tool=memcheck', '--leak-check=full', '--dsymutil=yes', '--log-file=valgrind.out']
+if args.tool == 'callgrind':
+    prefix = ['valgrind', '--tool=callgrind', '--log-file=callgrind.out']
 
 cmdline = prefix + [opp_run] + lib_flags + ned_flags + img_flags + omnet_args
 
 if args.verbose:
-    print "Running with command line arguments: %s" % ' '.join(['"%s"' % arg for arg in cmdline])
+    print("Running with command line arguments: %s" % ' '.join(['"%s"' % arg for arg in cmdline]))
 
-os.execvp('env', ['env'] + cmdline)
+if os.name == 'nt':
+    subprocess.call(['env'] + cmdline)
+else:
+    os.execvp('env', ['env'] + cmdline)
 
