bfdd4561541a17b1c3e0dbd55dedcc96fb422c86
Michele Segata <segata@ccs-labs.org>
2021-03-23 11:23:26 +0100

    add missing preprocessor directive

--- src/plexe/driver/Veins11pRadioDriver.h
+++ src/plexe/driver/Veins11pRadioDriver.h
@@ -18,6 +18,8 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
+#pragma once
+
 #include "veins/base/modules/BaseApplLayer.h"
 #include "plexe/driver/PlexeRadioDriverInterface.h"
 
