8793ef93e90959d250f68a32007087ae826e1939
Michele Segata <segata@ccs-labs.org>
2021-03-23 11:23:04 +0100

    enable veins 5.1 only

--- src/plexe/plexe.h
+++ src/plexe/plexe.h
@@ -29,6 +29,6 @@
 #define PLEXE_VERSION_ALPHA 3
 
 // Explicitly check Veins version number
-#if !(VEINS_VERSION_MAJOR == 5 && VEINS_VERSION_MINOR >= 0)
-#error Veins version 5.0 or compatible required
+#if !(VEINS_VERSION_MAJOR == 5 && VEINS_VERSION_MINOR >= 1)
+#error Veins version 5.1 or compatible required
 #endif
